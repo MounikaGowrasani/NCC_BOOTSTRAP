8925dd7934619f800a1fff77d8c3bc9030545482
Hemasri Yakkati <hemasri.yakkati1816@gmail.com>
2023-11-28 08:43:22 +0530

    hema

---
+++ CADET/logout.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+// Destroy the session to log out the user
+session_destroy();
+
+// Send a response
+echo "<script>alert('Logged out successfully');</script>";
+header("refresh:1;url=loginmain.php");
+exit; 
+?>
