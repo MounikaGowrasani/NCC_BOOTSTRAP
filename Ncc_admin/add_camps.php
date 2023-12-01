051b2dedf208ebef05dc0b64ad2271dfad4f5ac4
mounika <gowrasanimounika@gmail.com>
2023-12-01 14:04:51 +0530

    hema

--- Ncc_admin/add_camps.php
+++ Ncc_admin/add_camps.php
@@ -139,12 +139,7 @@ color: #f00;
 
     <ul class="sidebar-nav" id="sidebar-nav">
 
-      <li class="nav-item">
-        <a class="nav-link " href="index.php">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
+    <li class="nav-heading">Dashboard</li>
       <li class="nav-item">
         <a class="nav-link collapsed" href="schedule.php">
           <i class="bi bi-person"></i>
--- Ncc_admin/feed_back.php
+++ Ncc_admin/feed_back.php
@@ -140,13 +140,7 @@ color: #f00;
   <aside id="sidebar" class="sidebar">
 
     <ul class="sidebar-nav" id="sidebar-nav">
-
-      <li class="nav-item">
-        <a class="nav-link " href="index.php">
-          <i class="bi bi-grid"></i>
-          <span>Dashboard</span>
-        </a>
-      </li><!-- End Dashboard Nav -->
+    <li class="nav-heading">Dashboard</li>
       <li class="nav-item">
         <a class="nav-link collapsed" href="schedule.php">
           <i class="bi bi-person"></i>
